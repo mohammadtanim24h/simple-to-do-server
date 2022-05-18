477c0371f8630ee71931797b3abb2b16954595d0
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-05-19 05:35:29 +0600

    added mongodb to server

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware
 app.use(cors());
 app.use(express.json());
+
+// MongoDB
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7gneyjs.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('db connected');
+        const taskCollection = client.db("toDoApp").collection("tasks");
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+app.get("/", (req, res) => {
+    res.send("Simple to do server is up and running");
+});
+
+app.listen(port, () => {
+    console.log("Listening to simple to do at port,", port);
+});
